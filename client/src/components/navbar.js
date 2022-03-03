446447efcaa2561f9598742b2b696a46f0029475
Shakti Dewan <shaktiyakhha@gmail.com>
2022-03-03 17:25:27 +0545

    style

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,59 +1,44 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import {NavLink} from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import logo from "../res/images/coffee.png";
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div>
-            <div className="d-flex" ></div>
-            <div className="d-flex">
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to="/">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/about">
-                      About
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/contact">
-                      Contact
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/login">
-                      Login
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/signup">
-                      Sign In
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </div>
+          <NavLink className="navbar-brand" to="/">
+            {/* <img src={logo} alt="logo" height="5%" width="5%"/> */}
+          </NavLink>
+        
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">
+                  Sign Up
+                </NavLink>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
